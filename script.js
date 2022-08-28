248f147856eb737fea84d8142933335f69b67ea0
Arif Billah <98398506+ArifBillah6482@users.noreply.github.com>
2022-08-28 11:20:53 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+let userId;
+const removeData = () => {
+  axios
+    .delete(`https://contact-arifbillah.herokuapp.com/data/${userId}`)
+    .then(() => console.log())
+    .catch((err) => console.log(err));
+};
 axios.get("https://contact-arifbillah.herokuapp.com/data").then((res) => {
   res.data.users.map((data) => {
-    document.write(`<div class="container" style="  width: max-content;
+    userId = data.id;
+    document.write(`
+        <p class="idNumber"  style="display: inline-block; color: rgb(250, 30, 238)"></p>
+
+    <div class="container" style="  width: max-content;
     height: max-content;
     display: inline-block;
   padding: 10px;
@@ -11,8 +22,23 @@ axios.get("https://contact-arifbillah.herokuapp.com/data").then((res) => {
        <div class="name" style="padding:5px;">Name: <span>${data.name}</span></div>
        <div class="email" style="padding:5px;">Email: <span>${data.email}</span></div>
        <div class="msg" style="padding:5px;">Message: <span>${data.msg}</span></div>
+       <button
+          class="removeData"
+          onclick="removeData()"
+          style="
+          color: rgb(245, 245, 245);
+            background-color: rgb(35, 37, 66);
+            padding: 7px;
+            border: 1px solid rgb(150, 150, 150);
+          "
+        >
+          Delete
+        </button>
      </div>
          `);
+    document.querySelector(
+      ".idNumber"
+    ).textContent = `মেসেজ সংখ্যাঃ ${res.data.users.length} টি`;
     document.body.style.backgroundColor = "black";
     document.body.style.color = "rgb(245, 245, 245)";
     document.body.style.fontFamily = " Arial, Helvetica, sans-serif";
